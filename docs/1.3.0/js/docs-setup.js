9be5e96b79ee4490b8e350a890582bf03f9f45b2
aureliome <aurelio.me@gmail.com>
2016-07-08 12:56:26 +0200

    Version 1.3.0

---
+++ docs/1.3.0/js/docs-setup.js
@@ -0,0 +1,15 @@
+NG_DOCS={
+  "sections": {
+    "version": "API Reference"
+  },
+  "pages": [],
+  "apis": {
+    "version": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/version/main",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
